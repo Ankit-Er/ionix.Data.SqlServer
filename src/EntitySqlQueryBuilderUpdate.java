1c04c1058dcd4fab222972cb523d119f5f6ac2b4
Mehmet Goren <mehmetgoren@outlook.com>
2016-02-22 22:40:54 +0200

    1

--- src/EntitySqlQueryBuilderUpdate.java
+++ src/EntitySqlQueryBuilderUpdate.java
@@ -1,6 +1,6 @@
-import ionix.Data.EntityMetaData;
-import ionix.Data.EntitySqlQueryBuilder;
-import ionix.Data.SqlQuery;
+
+import ionix.Data.*;
+import ionix.Utils.Ext;
 
 import java.util.HashSet;
 
@@ -8,18 +8,57 @@ import java.util.HashSet;
 public class EntitySqlQueryBuilderUpdate implements EntitySqlQueryBuilder {
 
     private HashSet<String> updatedFields;
-    public HashSet<String> getUpdatedFields(){
+
+    public HashSet<String> getUpdatedFields() {
         return this.updatedFields;
     }
-    public void setUpdatedFields(HashSet<String> updatedFields){
+
+    public void setUpdatedFields(HashSet<String> updatedFields) {
         this.updatedFields = updatedFields;
     }
 
     @Override
-    public SqlQuery createQuery(Object entity, EntityMetaData entityMetaData) {
-       if (null == entity)
-           throw new IllegalArgumentException("entity is null");
+    public SqlQuery createQuery(Object entity, EntityMetaData metaData) {
+        if (null == entity)
+            throw new IllegalArgumentException("entity is null");
 
-        return null;
+        boolean updatedFieldsEnabled = !Ext.isEmptyList(this.updatedFields);
+
+        SqlQuery query = new SqlQuery();
+        StringBuilder sb = query.getText();
+
+        sb.append("UPDATE ")
+                .append(metaData.getTableName())
+                .append(" SET ");
+
+
+        for (FieldMetaData field : metaData.getFields()) {
+            SchemaInfo schema = field.getSchema();
+
+            if (schema.getIsKey())
+                continue;
+
+            if (schema.getDatabaseGeneratedOption() != StoreGeneratedPattern.None)
+                continue;
+
+            if (schema.getReadOnly())
+                continue;
+
+            if (updatedFieldsEnabled && !this.updatedFields.contains(schema.getColumnName()))
+                continue;
+
+            sb.append(schema.getColumnName())
+                    .append('=');
+
+            SqlQueryHelper.setColumnValue(DbValueSetter.Instance, query, field, entity);
+
+            sb.append(',');
+        }
+
+        sb.delete(sb.length() - 1, sb.length());
+
+        query.combine(SqlQueryHelper.createWhereSqlByKeys(metaData, entity));
+
+        return query;
     }
 }
